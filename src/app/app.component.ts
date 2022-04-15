c66e501eb337821a5de667af90ad625c241c4553
LopatinaAnna <an.lopatina050@gmail.com>
2022-04-15 17:44:57 +0300

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title: string = 'counter';
+  @Input() number: number = 0;
+
+  changeNumber(isIncrease: boolean){
+    if(isIncrease){
+      this.number += 1;
+    }
+    else{
+      this.number -= 1;
+    }
+  }
+}
